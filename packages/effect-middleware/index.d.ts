f2104cf3e0b5dc2ee77349a94218d1afa39ebdbf
nulliel <stephen.ribich@gmail.com>
2019-08-21 17:53:38 -0400

    declarations

--- packages/effect-middleware/index.d.ts
+++ packages/effect-middleware/index.d.ts
@@ -23,10 +23,10 @@ export interface EffectAPI {
 }
 
 // @ts-ignore
-export function createEffect<P>(actionCreator: ActionCreator, handler: EffectResolver<S, P>): void
-export function createEffect<Arg1, P>(actionCreator: ActionCreator1<Arg1, P>, handler: EffectResolver<S, P>): void
-export function createEffect<Arg1, Arg2, P>(actionCreator: ActionCreator2<Arg1, Arg2, P>, handler: EffectResolver<S, P>): void
-export function createEffect<Arg1, Arg2, Arg3, P>(actionCreator: ActionCreator3<Arg1, Arg2, Arg3, P>, handler: EffectResolver<S, P>): void
-export function createEffect<Arg1, Arg2, Arg3, Arg4, P>(actionCreator: ActionCreator4<Arg1, Arg2, Arg3, Arg4, P>, handler: EffectResolver<S, P>): void
-export function createEffect<Arg1, Arg2, Arg3, Arg4, Arg5, P>(actionCreator: ActionCreator5<Arg1, Arg2, Arg3, Arg4, Arg5, P>, handler: EffectResolver<S, P>): void
-export function createEffect<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, P>(actionCreator: ActionCreator6<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, P>, handler: EffectResolver<S, P>): void
+export function createEffect<P>(actionCreator: ActionCreator, handler: EffectResolver<P>): void
+export function createEffect<Arg1, P>(actionCreator: ActionCreator1<Arg1, P>, handler: EffectResolver<P>): void
+export function createEffect<Arg1, Arg2, P>(actionCreator: ActionCreator2<Arg1, Arg2, P>, handler: EffectResolver<P>): void
+export function createEffect<Arg1, Arg2, Arg3, P>(actionCreator: ActionCreator3<Arg1, Arg2, Arg3, P>, handler: EffectResolver<P>): void
+export function createEffect<Arg1, Arg2, Arg3, Arg4, P>(actionCreator: ActionCreator4<Arg1, Arg2, Arg3, Arg4, P>, handler: EffectResolver<P>): void
+export function createEffect<Arg1, Arg2, Arg3, Arg4, Arg5, P>(actionCreator: ActionCreator5<Arg1, Arg2, Arg3, Arg4, Arg5, P>, handler: EffectResolver<P>): void
+export function createEffect<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, P>(actionCreator: ActionCreator6<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, P>, handler: EffectResolver<P>): void
